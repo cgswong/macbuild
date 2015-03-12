7a3d884230b3da4083f9f3dc1edbd4ee43a1d44c
cgswong <cgs.wong@gmail.com>
2015-03-12 16:01:10 -0500

    Add Cloud Foundry CLI.

--- brewsetup.sh
+++ brewsetup.sh
@@ -32,9 +32,11 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-echo "Installing binaries..."
+echo "Setting taps..."
 brew tap homebrew/binary
 brew tap homebrew/dupes
+brew tap pivotal/tap
+echo "Installing binaries..."
 brew install ${binaries[@]}
 
 # Remove outdated versions from the cellar.
