3f8aa5781992653c9fe631277685c223ced0f12b
Stuart Wong <cgs.wong@gmail.com>
2015-06-23 08:29:57 -0500

    Remove pipeline fail, add echo output.

--- brewupdate.sh
+++ brewupdate.sh
@@ -4,13 +4,17 @@
 # DESC: Script to update, upgrade and check (doctor) Homebrew.
 # ############################################################################
 
-set -e
-
 terminal-notifier -title 'Homebrew' -message 'Updating and upgrading.'
+echo "Running 'brew update'."
 brew update
+echo "Running 'brew cask update'."
 brew cask update
+echo "Attempting to upgrade brew cask."
 brew upgrade brew-cask
+echo "Attempting to upgrade all brews."
 brew upgrade --all
+echo "Doing cleanup."
 brew cleanup
 brew cask cleanup
 brew doctor
+
